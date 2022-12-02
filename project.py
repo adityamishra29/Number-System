805dbc6dda312cde6f0a5e2deeb50df3d302d8e4
adityamishra29 <118502602+adityamishra29@users.noreply.github.com>
2022-12-02 23:01:34 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,4 +1,17 @@
-
+#In this project you have to enter a range [A, B] and system will randomly pick any number from
+#your given range and check the status of that given number.
+#The properties to be checked are:
+#1. Is that number is odd or even
+#2. Is that number is positive or negative number
+#3. Is that number is prime number or composite number.
+#After checking the status, you have to display all the properties followed by the randomly picked
+#number.
+#For example
+#Range is (1,12) and randomly picked number is 10
+#Then the properties followed by this number are:
+#10 is a positive number
+#10 is an even number
+#10 is a composite number
 
 import random
 a = int(input("The lower range: "))
